0c1a0471ec2d93e91701e97996e1327421d5129f
Hugo Peres <hugo-pdf@hotmail.com>
2020-12-11 07:30:40 -0300

    update tests

---
+++ app/models/camera_info.py
@@ -0,0 +1,17 @@
+class CameraInfo:
+
+    state = ''
+    cu = ''
+    popid = ''
+    parameter = ''
+    program = ''
+    program_index = ''
+    total_programs = ''
+    life_beat_cobot = ''
+    manual = ''
+    jobtime = ''
+    uptime = ''
+    message = ''
+
+    def __init__(self):
+        pass
